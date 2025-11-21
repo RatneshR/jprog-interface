64dd479824abe8b9fb44619d23ed74a6d98b64bd
Ratnesh Rai <ratnesh.rai26@gmail.com>
2025-11-21 22:35:58 +0530

    Initial Commit

---
+++ src/main/java/com/rr/jprog_interface/JprogInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.rr.jprog_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JprogInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JprogInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rr/jprog_interface/JprogInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rr.jprog_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JprogInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
